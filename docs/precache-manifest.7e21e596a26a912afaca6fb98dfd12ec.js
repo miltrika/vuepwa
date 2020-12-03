3ff4c7fd0c32c8c830963cb0ca45fbb51986c460
Kaupo <miltrika@live.com>
2020-12-03 20:35:16 +0200

    dist -> docks

---
+++ docs/precache-manifest.7e21e596a26a912afaca6fb98dfd12ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c243b9a962905635e2",
+    "url": "/css/app.3ff004a8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "958cffc50904503e61bd32b95ac8c3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/js/about.063687bd.js"
+  },
+  {
+    "revision": "91c243b9a962905635e2",
+    "url": "/js/app.c4300e88.js"
+  },
+  {
+    "revision": "46c97a951e383b757955",
+    "url": "/js/chunk-vendors.d50d084d.js"
+  },
+  {
+    "revision": "0cecec95db209a32781eb20aa8416046",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
